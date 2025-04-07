c5018c9402efebb213561fef1861e6e97644b999
thanhlamcode <lamdoan1122334455@gmail.com>
2025-04-07 15:19:30 +0700

    update db

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Cấu hình kết nối SQL Server
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://@localhost/ExpenseDB?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&charset=utf8"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://expensedb_plg9_user:5ZuAb9BqYPod5ofrJOYBc7jiJjlnlPZE@dpg-cuu3c6qj1k6c738i0h6g-a.oregon-postgres.render.com/expensedb_plg9"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://snoopy_db_user:IEy77io6bl9MN47eVrxumnEAu7pxd0Ai@dpg-cvpof6i4d50c73brl64g-a.singapore-postgres.render.com/snoopy_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
